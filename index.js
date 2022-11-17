49fe502169d8bcf81b0a326bfbce158082b7f91f
rohit-iwnl <rohitmanivel9@gmail.com>
2022-11-17 10:11:19 +0530

    signup

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express()
 const posts=require('./posts')
+const signin=require('./signup')
+
+let users=[];
 
 app.get('/posts',(req,res)=> {
     res.json(posts)
+})
+app.post('/signup',(req,res)=> {
+    const user=req.body;
+    users.push(user)
 })
 
 app.get('/',(req,res)=>{
