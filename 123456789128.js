70eb4de8d12f8c759321a30b635715f74e81fda7
rohit-iwnl <rohitmanivel9@gmail.com>
2022-11-17 10:39:16 +0530

    product json

---
+++ 123456789128.js
@@ -0,0 +1,7 @@
+module.exports=  {
+    "_id": "123456789128",
+    "age": 24,
+    "price": 447,
+    "product": 365,
+    "name": "Diann"
+  }
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const posts=require('./products')
+const product1 = require('/123456789128')
 
 let users=[];
 
 app.get('/products',(req,res)=> {
     res.json(posts)
+})
+app.get('/123456789128',(req,res)=> {
+    res.json(product1)
 })
 
 app.get('/',(req,res)=>{
